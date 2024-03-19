e25d4bb00db83b6235d352a5df913b4e168d9835
vipultiwari22 <tiwarivipul155@gmail.com>
2024-03-19 14:23:52 +0530

    update Project

--- Frontend/lms-fronted/src/Layouts/HomeLayout.jsx
+++ Frontend/lms-fronted/src/Layouts/HomeLayout.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
+import { useDispatch, useSelector } from "react-redux";
 function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // for checking if user is login
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  // for displaying the options
+
+  const role = useSelector((state) => state?.auth?.role);
+
   function changeWidth() {
     const drwaerSide = document.getElementsByClassName("drawer-side");
-    drwaerSide[0].style.width = 0;
+    drwaerSide[0].style.width = "auto";
   }
 
   function hideDrawer() {
-    const element = document.getElementsByClassName("drawer-togge");
+    const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
-    changeWidth();
+
+    const drwaerSide = document.getElementsByClassName("drawer-side");
+    drwaerSide[0].style.width = "0";
+  }
+  async function handleLogout(event) {
+    event.preventDefault();
+
+    // const res= await dispatch(logout())
+    // if(res?.payload?.success)
+    navigate("/");
   }
 
   return (
     <>
-      <div className="min-h-[90vh]">
-        <div className="drawer absolute left-0 z-50 w-0">
-          <input type="checkbox" className="drawer-toggle" id="my-drawer" />
+      <div className="min-h-[90vh] bg-gray-500">
+        <div className="drawer absolute left-0 z-50 w-fit">
+          <input className="drawer-toggle" id="my-drawer" type="checkbox" />
           <div className="drawer-content">
             <label htmlFor="my-drawer" className="cursor-pointer relative">
               <FiMenu
@@ -29,17 +49,22 @@ function HomeLayout({ children }) {
               />
             </label>
           </div>
-          <div className="drawer-side">
+          <div className="drawer-side w-0">
             <label htmlFor="my-drawer" className="drawer-overlay"></label>
-            <ul className="menu p-4 width-48 sm:w-80 bg-base-100 text-base-content relative">
+            <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
               <li className="w-fit absolute right-2 z-50">
-                <button>
-                  <AiFillCloseCircle onClick={hideDrawer} size={"24px"} />
+                <button onClick={hideDrawer}>
+                  <AiFillCloseCircle size={24} />
                 </button>
               </li>
               <li>
                 <Link to="/">Home</Link>
               </li>
+              {isLoggedIn && role === "ADMIN" && (
+                <li>
+                  <Link to="/admin/dashboard">Admin DashBoard</Link>
+                </li>
+              )}
               <li>
                 <Link to="/About">About Us</Link>
               </li>
@@ -49,10 +74,37 @@ function HomeLayout({ children }) {
               <li>
                 <Link to="/course">All Courses</Link>
               </li>
+
+              {!isLoggedIn && (
+                <li className="absolute bottom-4">
+                  <div className="w-full flex items-center justify-center">
+                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                      <Link to="/login">Login</Link>
+                    </button>
+                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                      <Link to="/signup">Signup</Link>
+                    </button>
+                  </div>
+                </li>
+              )}
+
+              {isLoggedIn && (
+                <li className="absolute bottom-4">
+                  <div className="w-full flex items-center justify-center">
+                    <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                      <Link to="/user/profile">Profile</Link>
+                    </button>
+                    <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                      <Link onClick={handleLogout}>Logout</Link>
+                    </button>
+                  </div>
+                </li>
+              )}
             </ul>
           </div>
         </div>
         {children}
+
         <Footer />
       </div>
     </>
--- Frontend/lms-fronted/src/Pages/HomePage.jsx
+++ Frontend/lms-fronted/src/Pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import HomeLayout from "../Layouts/HomeLayout";
+import { Link } from "react-router-dom";
+import HomePageimage from "../assets/images/homePageMainImage.png";
+
 function HomePage() {
   return (
     <HomeLayout>
@@ -14,6 +17,23 @@ function HomePage() {
             We have a large courses taught by highly skilled and quailyfied
             faculities at a very affordable cost{" "}
           </p>
+          <div className="space-x-6">
+            <Link to={"/courses"}>
+              <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                Expolre Courses
+              </button>
+            </Link>
+
+            <Link to={"/contact"}>
+              <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                Contact Us
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex items-center justify-center">
+          <img src={HomePageimage} alt="homepage image" />
         </div>
       </div>
     </HomeLayout>
